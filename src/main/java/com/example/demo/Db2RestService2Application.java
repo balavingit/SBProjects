c4e2b0de8f55e5947fd99ea4dc5b2aa03d5399e7
n0305214 <n0305214@VDDP35X-OKQ82SM.lm.lmig.com>
2018-07-19 16:51:02 -0400

    sdhfkjs

---
+++ src/main/java/com/example/demo/Db2RestService2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2RestService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db2RestService2Application.class, args);
+	}
+}
